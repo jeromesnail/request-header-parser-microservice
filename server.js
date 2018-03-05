e10da88ca5cb70bf5c137a1715d8c00bcbf62c0d
jeromesnail <jeromesnail@hotmail.fr>
2018-03-05 14:54:36 +0100

    fixed ip

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT;
+const port = '1337';
 
 app.use((req, res) => {
   const software = req.headers['user-agent'].match(/\([^)]*\)/g).shift().slice(1, -1);
   const lang = req.headers['accept-language'].split(',').shift();
-  const ipAdress = req.ip;
+  let ipAddress = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(',').shift();
+  if (ipAddress.substr(0, 7) == "::ffff:") {
+    ipAddress = ipAddress.substr(7)
+  }
   res.json({
-    'ipaddress': ipAdress,
+    'ipaddress': ipAddress,
     'language': lang,
     'software': software
   });
